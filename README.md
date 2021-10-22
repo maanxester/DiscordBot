4a7d6f09c1838821b3a41a0619402db14b02dc48
Matheus Nunes <maanxester21@hotmail.com>
2021-10-22 16:43:23 -0300

    Fundamentals DiscordBot

---
+++ README.md
@@ -0,0 +1,24 @@
+# Discord Bot
+
+Personal project creating Discord Bot with Python.
+
+## Installation
+
+Python3 and Pip3 is required.
+
+In Linux:
+```shell
+$ sudo apt install python3
+$ sudo apt install python3-pip
+```
+
+And install the requirements with **requirements.txt**:
+Inside the **DiscordBot** directory and inside terminal:
+
+```shell
+$ pip install -r requirements.txt
+
+export token=Insira aqui o token bot
+
+$ python3 DiscordBot.py
+```
--- app/DiscordBot.py
+++ app/DiscordBot.py
@@ -1,6 +1,7 @@
 import discord
 import os
-from discord.ext import commands
+import datetime
+from discord.ext import commands, tasks
 
 bot = commands.Bot(command_prefix="!")
 
@@ -8,6 +9,36 @@ bot = commands.Bot(command_prefix="!")
 @bot.event
 async def on_ready():
     print(f"Estou pronto para bagaçar! Estou conectado como {bot.user}")
+    current_time.start()
+
+@bot.event
+async def on_message(message):  # Evita loop
+    if message.author == bot.user:
+        return
+
+    if "Bot lixo" in message.content:
+        await message.channel.send(f"Por favor, {message.author.name}, eu não sou ruim.")
+
+    await bot.process_commands(message)
+
+
+@bot.command(name="oi")
+async def send_hello(ctx):
+    name = ctx.author.name
+    response = "Olá, " + name
+
+    await ctx.send(response)
+
+
+@tasks.loop(hours=1)
+async def current_time():
+    now = datetime.datetime.now()
+    now = now.strftime("%d/%m/%Y às %H:%M:%S")
+
+    channel = bot.get_channel(856956765216047124)  # ID canal abbot
+
+    await channel.send("Data atual: " + now)
+
 
 # Token do Bot
 bot.run(os.getenv("token"))  # Setar em variavel de ambiente
